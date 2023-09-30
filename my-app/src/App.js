8b6f627ccc62a85d520453a7779690b6f97b7b6b
chetan milmile <107764112+Chetanmilmile@users.noreply.github.com>
2023-09-30 11:52:50 +0530

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <label className="chetan">
+    Name:
+    <input className="amit" type="text" name="name" />
+        </label>
+  <input className="gogo" type="submit" value="Submit" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
